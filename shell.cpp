3905b2b7aed50ca797e5950658ae15948dc0b0b3
burak <rburaksaritas@gmail.com>
2022-10-23 16:36:18 +0300

    Add feature to exit program
    Program ends when the input "exit" is given.

--- shell.cpp
+++ shell.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <unistd.h>
 #include <limits.h>
 
-
+// Gets the username of user and anchor >>>.
 int printUserName(){
     char username[LOGIN_NAME_MAX];
     getlogin_r(username, LOGIN_NAME_MAX);
@@ -11,19 +11,33 @@ int printUserName(){
     return 1;
 }
 
+// Gets input command from commandline.
 std::string getCommandInput(){
     std::string command;
-    std::cin >> command;
+    std::getline(std::cin, command);
     return command;
 }
 
+// Checks if the input command equals to "exit".
+// If so, sets isRunning to false in order to stop program. 
+int checkExit(std::string command, bool &isRunning){
+    std::string exitCommand = "exit";
+    if (command == exitCommand){
+        isRunning = false;
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
+    // Boolean to control the program running state.
     bool isRunning = true;
     
     while (isRunning)
     {
         printUserName();
-        getCommandInput();
+        std::string command = getCommandInput();
+        checkExit(command, isRunning);
     }
     
     return 0;
