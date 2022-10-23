855538ff212a258b6cc0c68d2ed78cd1b5fda812
burak <rburaksaritas@gmail.com>
2022-10-23 15:52:20 +0300

    Add feature to get username

--- shell.cpp
+++ shell.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+#include <limits.h>
+
 
 int main() {
-    std::string command;
-    std::cout << "Hello World!\n";
+    char username[LOGIN_NAME_MAX];
+    getlogin_r(username, LOGIN_NAME_MAX);
+    std::cout << username << " >>> ";
     return 0;
 }
-
-int 
