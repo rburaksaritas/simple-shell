b60ff75ca46aca6dbfc48c9cbdf9492ed604b203
burak <rburaksaritas@gmail.com>
2022-10-25 01:11:53 +0300

    Add comments

--- shell.cpp
+++ shell.cpp
@@ -6,7 +6,12 @@
 #include <vector>
 #include <algorithm>
 
-// Gets the username of user and anchor >>>.
+/**
+ * @author Ramazan Burak Saritas
+ * 2020400321
+*/
+
+// Gets and prints the username of user followed by anchor >>>.
 int printUserName(){
     char username[LOGIN_NAME_MAX];
     getlogin_r(username, LOGIN_NAME_MAX);
@@ -14,7 +19,7 @@ int printUserName(){
     return 1;
 }
 
-// Gets input command from commandline.
+// Gets input command from commandline as string.
 std::string getCommandInput(){
     std::string command;
     std::getline(std::cin, command);
@@ -83,14 +88,14 @@ int whatsMyIp(){
 // and write them on the standard output, line by line. 
 int printFile(std::string fileName){
     std::string commandLine = std::string("while IFS= read -r line\n") +
-    std::string("do\n") + 
-    std::string("   echo -en \033[1A\n") +  // Prevent blank lines between lines.
-    std::string("   read input </dev/tty\n") + // Enter input.
-    std::string("    if [[ -z $input ]]\n") +
-    std::string("    then\n") +
-    std::string("        echo $line\n") +   // Print line.
-    std::string("    fi\n") +
-    std::string("done < ") + fileName;
+        std::string("do\n") + 
+        std::string("   echo -en \033[1A\n") +  // Prevent blank lines between lines.
+        std::string("   read input </dev/tty\n") + // Enter input.
+        std::string("    if [[ -z $input ]]\n") +
+        std::string("    then\n") +
+        std::string("        echo $line\n") +   // Print line.
+        std::string("    fi\n") +
+        std::string("done < ") + fileName;
     system(commandLine.c_str());
     return 1;
 }
@@ -98,6 +103,7 @@ int printFile(std::string fileName){
 // Takes a file name as an argument, and redirect the standard output
 // to a new file using the redirection operator, >, (greater than symbol).
 int printFile(std::string sourceFile, std::string newFile){
+    // Generates command text "cp (source) (new)" to run on system.
     std::string commandLine = "cp " + sourceFile + " " + newFile;
     system(commandLine.c_str());
     return 1;
@@ -106,7 +112,8 @@ int printFile(std::string sourceFile, std::string newFile){
 // Searches a simple command in up to maximum 15 commands 
 // that are executed before. The output is a simple "Yes" or "No".
 std::string didIDoThat(std::string commandLine, std::vector<std::string> history){
-if (std::find(history.begin(), history.end(), commandLine) != history.end()){
+    // Uses find() from <algorithm> library to see if the given command exists in history.
+    if (std::find(history.begin(), history.end(), commandLine) != history.end()){
         return "Yes";
     } else{
         return "No";
@@ -115,6 +122,7 @@ if (std::find(history.begin(), history.end(), commandLine) != history.end()){
 
 // Opens the default text editor.
 int helloText(){
+    // Creates a new file named untitled.txt and opens it with default text editor.
     std::string commandLine = "touch untitled.txt\n xdg-open untitled.txt";
     system(commandLine.c_str());
     return 1;
@@ -127,6 +135,7 @@ int main() {
     // Vector to store command history.
     std::vector<std::string> history;
 
+    // Command strings to match them with commands.
     std::string listdirCommand = "listdir";
     std::string mycomputernameCommand = "mycomputername";
     std::string whatsmyipCommand = "whatsmyip";
@@ -141,16 +150,19 @@ int main() {
         printUserName();
         std::string commandInput = getCommandInput();
         std::vector<std::string> parsedInput = parseCommandInput(commandInput);
+        // Sets the command part from the given input command line.
         std::string command = parsedInput[0];
-        // Push command line to history.
+        // Pushes current command line to history as a whole string.
         history.push_back(commandInput);
-        // Remove first element if the size exceeds 15.
+        // Remove first element of history if the size exceeds 15.
         if (history.size()>15){
             history.erase(history.begin());
         }        
-
+        // Checks if the given commands matches exit command.
         checkExit(command, isRunning);
 
+        // Checks if the given command matches any defined command.
+        // Executes related function if it matches.
         if (command == listdirCommand){
             listDir();
         } else if (command == mycomputernameCommand){
@@ -158,16 +170,20 @@ int main() {
         } else if (command == whatsmyipCommand){
             whatsMyIp();
         } else if (command == printfileCommand){
+            // If the printfile has one argument, 
+            // executes the printFile with one parameter.
             if (parsedInput.size() == 2){
                 std::string fileName = parsedInput[1];
                 printFile(fileName);
+            // If the command has form printfile (source) > (new), 
+            // executes the printFile with two parameters.
             } else if (parsedInput.size() == 4){
                 std::string sourceFile = parsedInput[1];
                 std::string newFile = parsedInput[3];
                 printFile(sourceFile, newFile);
             }
         } else if (command == dididothatCommand){
-            // remove dididothat part and quotes from input string.
+            // remove dididothat part and quotes from whole input string.
             std::string givenCommand = commandInput.substr(12, commandInput.length()-12);
             // remove last quote.
             givenCommand.pop_back();
